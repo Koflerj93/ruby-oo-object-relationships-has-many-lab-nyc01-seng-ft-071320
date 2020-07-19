b8c4e3ec970c0e5814bc46fe51f7f042fe4eda98
Joseph Kofler <koflerj93@gmail.com>
2020-07-19 23:07:58 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,5 +6,23 @@ class Artist
     @name = name
   end 
   
+  def songs
+    Post.all.select do |post|
+      post.author = self
+    end 
+  end 
+    
+    def add_post(post)
+      post.author = self
+    end 
+    
+    def add_post_by_title(title) 
+      post = Post.new(title)
+      post.author = self
+    end 
+    
+    def self.post_count
+      Post.all.count
+    end 
 
 end
